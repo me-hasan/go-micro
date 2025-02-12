45a8892d25f0d56ebcff9cf3433d2aaed83dbe06
Md Khayrul Hasan <khayrul.web@gmail.com>
2025-02-12 21:25:22 +0600

    mail server write functionality and configuration

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Mailer Mail
+}
+
+const webPort = "80"
+
+func main() {
+	app := Config{
+		Mailer: createMailer(),
+	}
+
+	log.Println("Starting mail service on port", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func createMailer() Mail {
+	mailPort, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:      os.Getenv("DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        mailPort,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+		FromName:    os.Getenv("FROM_NAME"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+	}
+
+	return m
+}
