9c2dd3a3dec87fabe37bcb81d4549f5d41722367
Md Khayrul Hasan <khayrul.web@gmail.com>
2025-02-13 22:01:44 +0600

    Rabbitmq connect to listener service

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 func main() {
 	// try to connect to rabbitmq
+	rabbitMQConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitMQConn.Close()
+	log.Println("Connected to RabbitMQ")
 
 	// start listing for messages
 
@@ -9,4 +26,34 @@ func main() {
 
 	// watch for messages and consume events
 
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ not ready yet, retrying in 1 second...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
 }
